22dfe2ceda7868f7e77fb065d63c11588d33add1
Cody Harrington <cody@harringtonca.com>
2015-07-13 19:30:27 +1200

    Added some more code changes

---
+++ src/nz/co/wholemeal/christchurchmetro/RenamePlatformActivity.java
@@ -0,0 +1,28 @@
+package nz.co.wholemeal.christchurchmetro;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+public class RenamePlatformActivity extends Dialog {
+
+    public RenamePlatformActivity(Context context) {
+       super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+}
